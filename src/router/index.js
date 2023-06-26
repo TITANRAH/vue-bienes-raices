c0f1c5732990036d796a5d73e3eae7017e1f958c
sergio <granrah1@gmail.com>
2023-06-26 14:32:30 -0400

    avance

--- src/router/index.js
+++ src/router/index.js
@@ -2,7 +2,7 @@ import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
 import { onAuthStateChanged } from 'firebase/auth'
 import { useFirebaseAuth } from 'vuefire'
-USEORI
+
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
